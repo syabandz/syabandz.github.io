e27c510a8869e397d84a5605e1ab8a586674b1f5
Mawardi Sya'ban <19517968+syabandz@users.noreply.github.com>
2018-03-07 14:49:26 +0700

    Add files via upload

---
+++ index_2.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Portofolio :: Sya'ban</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Trendy CV Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+	SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link rel="icon" type="image/x-icon" href="images/sya.png"/>
+<link href="css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
+<link href="css/style.css" type="text/css" rel="stylesheet" media="all">   
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<script src="js/jquery-2.2.3.min.js"></script>  
+<link href="//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet">
+</head>
+<body> 
+	<div class="banner">
+		<div class="banner-w3lsinfo"> 
+			<div class="header">
+				<div class="container">
+					<div class="logo">
+						<h1 class="col-md-12 banner-top-right wow zoomInUp animated" data-wow-delay=".10s" style="font-family: Niconne;">Mawardi Sya'ban</h1>
+					</div> 
+					<div class="menu">
+						<a href="#" class="navicon"></a> 
+						<div class="toggle effect-3"> 
+							<ul class="toggle-menu">
+								<li><a href="index_2.php" class="active"> Beranda</a></li>
+								<li><a href="projects_2.php"  > Proyek Saya </a></li>
+								<li><a href="index.php"> Bahasa </a></li>							
+							</ul>
+						</div> 
+					</div>   
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+			<div class="banner-text"> 
+				<div class="container"> 
+		        <p class="wow zoomInDown animated" data-wow-delay=".10s"><img src="images/sya.png" alt="" class="img-circle"></p>
+				</div>
+			</div>
+			<div class="w3agile-address"> 
+			<div class="banner-w3lstext">
+				<div class="container" style="font-family: Niconne;"> 
+                    <ul><li class="wow slideInLeft animated" data-wow-delay=".10s"><i class="fa fa-map-marker"></i><a href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.0358329452706!2d106.82077181405771!3d-6.389377864271214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69ebfc9b089ca3%3A0x38a1312d27faae15!2sGg.+Irigasi%2C+Kemiri+Muka%2C+Beji%2C+Kota+Depok%2C+Jawa+Barat+16423!5e0!3m2!1sid!2sid!4v1518361570727" width="350" height="150" frameborder="0" style="border:0" allowfullscreen target="_blank">Kp.Bulak RT.02/13 No.39 Kec.Beji Kota Depok</a></li></ul>
+					<ul><li class="wow slideInRight animated" data-wow-delay=".10s"><i class="fa fa-phone"></i> +62 857 7948 6707 </li></ul>
+					<ul><li class="wow slideInLeft animated" data-wow-delay=".10s"><i class="fa fa-envelope"></i> syaban.mawardi@gmailcom </li></ul>
+					</div>
+				</div> 
+			</div> 
+		</div>
+	</div>
+	<div class="about">
+		<div class="container">
+			<div class="w3ls-title">
+				<h2 class="agileits-title wow zoomInUp animated" data-wow-delay=".10s">Tentang Saya</h2>
+			</div>
+			<div class="about-agileinfo"> 
+				<div class="col-md-4 col-sm-4 about-left ">
+					<img src="images/14.jpg" class="img-responsive wow bounceInRight animated" data-wow-delay=".10s" alt=""/>
+					<div class="social-wthree-icons wow fadeInLeft animated" data-wow-delay=".10s">
+						<ul>
+							<center><li><a href="https://plus.google.com/103657623572697571664" class="fa fa-google-plus icon icon-border googleplus" title="Google Plus" target="_blank"> </a></li>
+							<li><a href="https://www.linkedin.com/in/syabandz" class="fa fa-linkedin icon icon-border twitter" title="Linkedin" target="_blank"> </a></li>
+							<li><a href="https://www.github.com/syabandz" class="fa fa-github icon icon-border googleplus" title="Github" target="_blank"> </a></li>
+						</center></ul>
+					</div>
+				</div> 
+				<div class="col-md-8 col-sm-8 about-right wthree wow fadeInRight animated" data-wow-delay=".10s" style="font-family: Niconne;">
+					<h3>Hai, Saya adalah <span>Mawardi Sya'ban</span></h3>
+					<h4>Pengembang Web Junior </h4>
+					<ul class="address">
+						<li>
+							<ul class="agileits-address-text ">
+								<li><b>TGL. LAHIR</b></li>
+								<li>:&nbsp;&nbsp;22 Maret 1990</li>
+							</ul>
+						</li>
+						<li>
+							<ul class="agileits-address-text">
+								<li><b>JENIS KELAMIN</b></li>
+								<li>:&nbsp;&nbsp;Laki - laki</li>
+							</ul>
+						</li>
+						<li>
+							<ul class="agileits-address-text">
+								<li><b>AGAMA</b></li>
+								<li>:&nbsp;&nbsp;Islam</li>
+							</ul>
+						</li>
+						<li>
+							<ul class="agileits-address-text">
+								<li><b>ALAMAT</b></li>
+								<li>:&nbsp;&nbsp;Kp.Bulak RT.02/13 Kec.Beji Depok</li>
+							</ul>
+						</li>
+						<li>
+							<ul class="agileits-address-text">
+								<li><b>NO HP</b></li>
+								<li>:&nbsp;&nbsp; +62 857 7948 6707 </li>
+							</ul>
+						</li>
+						<li>
+							<ul class="agileits-address-text">
+								<li><b>E-MAIL</b></li>
+								<li>:&nbsp;&nbsp;syaban.mawardi@gmailcom </li>
+							</ul>
+						</li>
+						<li>
+							<ul class="agileits-address-text">
+								<li><b>KULIAH</b></li>
+								<li>:&nbsp;&nbsp;STMIK Pranata Indonesia Bekasi </li>
+							</ul>
+						</li>
+						<li>
+							<ul class="agileits-address-text">
+								<li><b>JURUSAN</b></li>
+								<li>:&nbsp;&nbsp;Teknik Informatika </li>
+							</ul>
+						</li>
+					</ul> 
+					<div class="hire-w3lgrids">
+						<a href="images/cv_syaban.pdf" target="_blank" class="wthree-more nina" data-text="UNDUH CV"> 
+							<span>U</span><span>n</span><span>d</span><span>u</span><span>h</span> <span>c</span><span>v</span>					
+						</a> 
+					</div></div></div>  
+					<div class="col-md-12 col-sm-12 about-right wthree wow fadeInRight animated" data-wow-delay=".10s">
+					<div class="about skills" style="margin-top: -50px">
+						<h4>Keterampilan Teknis</h4>
+						<div class="skillbar" data-percent="80">
+							<span class="skillbar-title" style="background:#0396d8;">HTML5</span>
+							<p class="skillbar-bar" style="background: #03A9F4;"></p>
+							<span class="skill-bar-percent"></span>
+						</div>
+						<div class="skillbar" data-percent="75">
+							<span class="skillbar-title" style="background: #028679;">PHP</span>
+							<p class="skillbar-bar" style="background: #009688;"></p>
+							<span class="skill-bar-percent"></span>
+						</div>
+						<div class="skillbar" data-percent="70">
+							<span class="skillbar-title" style="background:darkorange;">MySQL</span>
+							<p class="skillbar-bar" style="background: orange;"></p>
+							<span class="skill-bar-percent"></span>
+						</div>
+						<div class="skillbar" data-percent="60">
+							<span class="skillbar-title" style="background:#9226a4;">CSS3</span>
+							<p class="skillbar-bar" style="background:#b32eca;"></p>
+							<span class="skill-bar-percent"></span>
+						</div>
+						<div class="skillbar" data-percent="40">
+							<span class="skillbar-title" style="background:darkgreen;">Javascript</span>
+							<p class="skillbar-bar" style="background:green;"></p>
+							<span class="skill-bar-percent"></span>
+						</div>
+						<div class="skillbar" data-percent="35">
+							<span class="skillbar-title" style="background:darkblue;">jQuery</span>
+							<p class="skillbar-bar" style="background:blue;"></p>
+							<span class="skill-bar-percent"></span>
+						</div>
+						<div class="skillbar" data-percent="35">
+							<span class="skillbar-title" style="background:darkred;">Ajax</span>
+							<p class="skillbar-bar" style="background:red;"></p>
+							<span class="skill-bar-percent"></span>
+						</div>
+				</div> 
+			</div> 
+		</div>
+	</div></div></div></div></div>
+	<div class="w3agile-footer">
+		<div class="container" style="font-family: Niconne;"> 
+			<p>© <?php echo date('Y') ?> Trendy CV. All rights reserved | Design by <a href="http://syabandz.blogspot.com" target="_blank">syabandz.blogspot.com.</a></p>
+		</div>
+	</div>
+	<script src="js/skill.bars.jquery.js"></script> 
+	<script> 
+	$(document).ready(function(){		
+		$('.skillbar').skillBars({
+			from: 0,
+			speed: 4000, 
+			interval: 100,
+			decimals: 0,
+		});		
+	}); 
+	</script> 
+	<script>
+		$('.navicon').on('click', function (e) {
+		  e.preventDefault();
+		  $(this).toggleClass('navicon--active');
+		  $('.toggle').toggleClass('toggle--active');
+		});
+	</script> 
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
---
+++ projects.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Portofolio :: Sya'ban</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Trendy CV Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+	SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link rel="icon" type="image/x-icon" href="images/sya.png"/>
+<link href="css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
+<link href="css/style.css" type="text/css" rel="stylesheet" media="all">   
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<script src="js/jquery-2.2.3.min.js"></script>  
+<link href="//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet">
+</head>
+<body> 
+	<div class="banner">
+		<div class="banner-w3lsinfo"> 
+			<div class="header">
+				<div class="container">
+					<div class="logo">
+						<h1 class="col-md-12 banner-top-right wow zoomInUp animated" data-wow-delay=".10s" style="font-family: Niconne;">Mawardi Sya'ban</h1>
+					</div> 
+					<div class="menu">
+						<a href="#" class="navicon"></a> 
+						<div class="toggle effect-3"> 
+							<ul class="toggle-menu">
+								<li><a href="index.php"> Home</a></li>
+								<li><a href="projects.php"  class="active"> My Projects</a></li>							
+								<li><a href="index_2.php"> Language </a></li>							
+							</ul>
+						</div> 
+					</div>   
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+			<div class="banner-text"> 
+				<div class="container"> 
+		        <p class="wow zoomInDown animated" data-wow-delay=".10s"><img src="images/sya.png" alt="" class="img-circle"></p>
+				</div>
+			</div>
+			<div class="w3agile-address"> 
+			<div class="banner-w3lstext">
+				<div class="container" style="font-family: Niconne;"> 
+                    <ul><li class="wow slideInLeft animated" data-wow-delay=".10s"><i class="fa fa-map-marker"></i><a href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.0358329452706!2d106.82077181405771!3d-6.389377864271214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69ebfc9b089ca3%3A0x38a1312d27faae15!2sGg.+Irigasi%2C+Kemiri+Muka%2C+Beji%2C+Kota+Depok%2C+Jawa+Barat+16423!5e0!3m2!1sid!2sid!4v1518361570727" width="350" height="150" frameborder="0" style="border:0" allowfullscreen target="_blank">Kp.Bulak RT.02/13 No.39 Kec.Beji Kota Depok</a></li></ul>
+					<ul><li class="wow slideInRight animated" data-wow-delay=".10s"><i class="fa fa-phone"></i> +62 857 7948 6707 </li></ul>
+					<ul><li class="wow slideInLeft animated" data-wow-delay=".10s"><i class="fa fa-envelope"></i> syaban.mawardi@gmailcom </li></ul>
+					</div>
+				</div> 
+			</div> 
+		</div>
+	</div>
+	<div class="about projects">
+		<div class="container">
+			<div class="w3ls-title">
+				<h2 class="agileits-title wow zoomInUp animated" data-wow-delay=".10s">My Projects</h2>
+			</div>
+			<div class="w3ls_gallery_grids" style="font-family: Niconne; font-size: 20px">	
+				<div class="col-xs-4 agileits_w3layouts_gallery_grid">	
+					<p class="wow zoomInRight animated" data-wow-delay=".10s">DEMO : <a href="http://perpustakaan00.000webhostapp.com" target="_blank">Click Here !</a><br>username : admin <br> password : admin123!</p>
+					<div class="agileits_w3layouts_gallery_grid1 hover14 column">
+						<div class="w3_agile_gallery_effect view view-tenth">
+							<a href="images/utama.png" class="showcase" data-rel="lightcase:myCollection:slideshow" title="My Projects">
+								<img src="images/utama.png" alt=" " class="img-responsive wow flipInY animated" data-wow-delay=".10s" />
+								<div class="mask">
+									<h3>Sistem Informasi Perpustakaan Sekolah SMK Farmasi Sari Farma Depok</h3>
+									<p></p>
+								</div>
+							</a>
+						</div>
+					</div>
+					<div class="agileits_w3layouts_gallery_grid1 hover14 column">
+						<div class="w3_agile_gallery_effect view view-tenth">
+							<a href="images/buku_cetak.png" class="showcase" data-rel="lightcase:myCollection:slideshow" title="My Projects">
+								<img src="images/buku_cetak.png" alt=" " class="img-responsive wow flipInX animated" data-wow-delay=".10s" />
+								<div class="mask">
+									<h3>Sistem Informasi Perpustakaan Sekolah SMK Farmasi Sari Farma Depok</h3>
+									<p></p>
+								</div>
+							</a>
+						</div>
+					</div>
+					<div class="agileits_w3layouts_gallery_grid1 hover14 column">
+						<div class="w3_agile_gallery_effect view view-tenth">
+							<a href="images/backup.png" class="showcase" data-rel="lightcase:myCollection:slideshow" title="My Projects">
+								<img src="images/backup.png" alt=" " class="img-responsive wow lightSpeedIn animated" data-wow-delay=".10s" />
+								<div class="mask">
+									<h3>Sistem Informasi Perpustakaan Sekolah SMK Farmasi Sari Farma Depok</h3>
+									<p></p>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-4 agileits_w3layouts_gallery_grid">	
+					<div class="agileits_w3layouts_gallery_grid1 hover14 column">
+					<p class="wow bounceInUp animated" data-wow-delay=".10s">DEMO : <a href="http://lamaran2017.000webhostapp.com" target="_blank">Click Here !</a><br>username : 20170001 <br> password : 123456</p>
+						<div class="w3_agile_gallery_effect view view-tenth">
+							<a href="images/info.png" class="showcase" data-rel="lightcase:myCollection:slideshow" title="My Projects">
+								<img src="images/info.png" alt=" " class="img-responsive wow rotateInDownLeft animated" data-wow-delay=".10s" />
+								<div class="mask">
+									<h3>Sistem Informasi Lowongan Kerja Pada PT Samindo Electronics Bekasi</h3>
+									<p></p>
+								</div>
+							</a>
+						</div>
+					</div> 
+					<div class="agileits_w3layouts_gallery_grid1 hover14 column">
+						<div class="w3_agile_gallery_effect view view-tenth">
+							<a href="images/info_2.png" class="showcase" data-rel="lightcase:myCollection:slideshow" title="My Projects">
+								<img src="images/info_2.png" alt=" " class="img-responsive wow rollIn animated" data-wow-delay=".10s" />
+								<div class="mask">
+									<h3>Sistem Informasi Lowongan Kerja Pada PT Samindo Electronics Bekasi</h3>
+									<p></p>
+								</div>
+							</a>
+						</div>
+					</div> 
+					<div class="agileits_w3layouts_gallery_grid1 hover14 column">
+						<div class="w3_agile_gallery_effect view view-tenth">
+							<a href="images/2.png" class="showcase" data-rel="lightcase:myCollection:slideshow" title="My Projects">
+								<img src="images/2.png" alt=" " class="img-responsive wow rotateInDownRight animated" data-wow-delay=".10s" />
+								<div class="mask">
+									<h3>Sistem Informasi Lowongan Kerja Pada PT Samindo Electronics Bekasi</h3>
+									<p></p>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-4 agileits_w3layouts_gallery_grid">	
+					<div class="agileits_w3layouts_gallery_grid1 w3layouts_gallery_grid1 hover14 column">
+					<p class="wow zoomInLeft animated" data-wow-delay=".10s">DEMO : <a href="http://persediaan2017.000webhostapp.com" target="_blank">Click Here !</a><br>username : ADM-001 <br> password : 123456</p>
+						<div class="w3_agile_gallery_effect view view-tenth">
+							<a href="images/barang_2.png" class="showcase" data-rel="lightcase:myCollection:slideshow" title="My Projects">
+								<img src="images/barang_2.png" alt=" " class="img-responsive wow rotateInUpLeft animated" data-wow-delay=".10s" />
+								<div class="mask">
+									<h3>Sistem Informasi Persediaan Barang Pada PT Unggul Cipta Piranti Jakarta</h3>
+									<p></p>
+								</div>
+							</a>
+						</div>
+					</div>
+					<div class="agileits_w3layouts_gallery_grid1 hover14 column">
+						<div class="w3_agile_gallery_effect view view-tenth">
+							<a href="images/3.png" class="showcase" data-rel="lightcase:myCollection:slideshow" title="My Projects">
+								<img src="images/3.png" alt=" " class="img-responsive wow bounceInRight animated" data-wow-delay=".10s" />
+								<div class="mask">
+									<h3>Sistem Informasi Persediaan Barang Pada PT Unggul Cipta Piranti Jakarta</h3>
+									<p></p>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div> 
+		</div>
+	</div>
+	<div class="w3agile-footer">
+		<div class="container" style="font-family: Niconne;"> 
+			<p>© <?php echo date('Y') ?> Trendy CV. All rights reserved | Design by <a href="http://syabandz.blogspot.com" target="_blank">syabandz.blogspot.com.</a></p>
+		</div>
+	</div>
+	<script src="js/lightcase.js"></script>
+	<script src="js/jquery.events.touch.js"></script>
+	<script>
+		$('.showcase').lightcase();
+	</script>
+	<script>
+		$('.navicon').on('click', function (e) {
+		  e.preventDefault();
+		  $(this).toggleClass('navicon--active');
+		  $('.toggle').toggleClass('toggle--active');
+		});
+	</script> 
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
